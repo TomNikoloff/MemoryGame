04154209f2e5bb2512d7a71d354ac6007ea9a130
TomNikoloff <tomnikoloff@hotmail.co.uk>
2022-02-21 21:04:27 +0000

    testing

--- memoryGame.js
+++ memoryGame.js
@@ -4,19 +4,52 @@ let cardCover = document.querySelectorAll('.cardCover');
 let cards = document.querySelectorAll('.card');
 let testDiv = document.querySelector('#testDiv');
 let testCard = document.querySelector('#test');
+let result = document.querySelector('#memGame-Result');
 const imgArray = ['img/lolaOne.jpg','img/lolaTwo.jpg', 'img/lolaTHree.jpg'];
+
 
 function randomNum() {
     return Math.floor(Math.random() * 3);
 }
 
-function memoryGame() {
+function generateCards() {
     for (let i = 0; cards.length; i++) {
         cards[i].src = imgArray[randomNum()];
     }
 }
 
-genCards.addEventListener('click', memoryGame);
+genCards.addEventListener('click', generateCards);
+
+
+
+
+function memoryGame(cardOne, cardTwo) {
+    let firstCard, secondCard;
+
+    firstCard
+    secondCard
+}
+
+function memGameFrontEnd(card) {
+    
+}
+
+function gameResult(firstCard, secondCard) {
+    if (firstCard.src === secondCard.src) {
+        result.appendChild(document.createTextNode('You Win'));
+    } else {
+        result.appendChild(document.createTextNode('You Lost'));
+    }
+}
+/*
+for (let i = 0; i < cardCover.length; i++) {
+    cardCover[i].addEventListener('click', displayCard);
+};
+
+function displayCard() {
+    this.
+}
+*/
 
 cardCover[0].addEventListener('click', function() {
     cards[0].style.zIndex = 100;
@@ -41,4 +74,3 @@ cardCover[4].addEventListener('click', function() {
 cardCover[5].addEventListener('click', function() {
     cards[5].style.zIndex = 100;
 });
-
