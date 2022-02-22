0c334148ae202e91c52afd896694bf39a79b2561
TomNikoloff <tomnikoloff@hotmail.co.uk>
2022-02-22 20:24:29 +0000

    Basic Styling done. Cards display on click.

--- memoryGame.js
+++ memoryGame.js
@@ -1,13 +1,17 @@
 
 const genCards = document.querySelector('#genBtn');
-let cardCover = document.querySelectorAll('.cardCover');
+let cardCover = document.getElementsByClassName('cardCover');
 let cards = document.querySelectorAll('.card');
 let testDiv = document.querySelector('#testDiv');
 let testCard = document.querySelector('#test');
 let result = document.querySelector('#memGame-Result');
 const imgArray = ['img/lolaOne.jpg','img/lolaTwo.jpg', 'img/lolaTHree.jpg'];
 
+let cardCovers = [...cardCover];
 
+let allCards = [...cards];
+
+/*
 function randomNum() {
     return Math.floor(Math.random() * 3);
 }
@@ -19,20 +23,10 @@ function generateCards() {
 }
 
 genCards.addEventListener('click', generateCards);
+*/
 
 
-
-
-function memoryGame(cardOne, cardTwo) {
-    let firstCard, secondCard;
-
-    firstCard
-    secondCard
-}
-
-function memGameFrontEnd(card) {
-    
-}
+/*
 
 function gameResult(firstCard, secondCard) {
     if (firstCard.src === secondCard.src) {
@@ -41,36 +35,16 @@ function gameResult(firstCard, secondCard) {
         result.appendChild(document.createTextNode('You Lost'));
     }
 }
-/*
-for (let i = 0; i < cardCover.length; i++) {
-    cardCover[i].addEventListener('click', displayCard);
-};
 
-function displayCard() {
-    this.
-}
 */
 
-cardCover[0].addEventListener('click', function() {
-    cards[0].style.zIndex = 100;
-});
+for (let i = 0; i < cardCover.length; i++) {
+   cardCover[i].addEventListener('click', displayCard);
+};
 
-cardCover[1].addEventListener('click', function() {
-    cards[1].style.zIndex = 100;
-});
 
-cardCover[2].addEventListener('click', function() {
-    cards[2].style.zIndex = 100;
-});
 
-cardCover[3].addEventListener('click', function() {
-    cards[3].style.zIndex = 100;
-});
+function displayCard() {
+    this.classList.toggle("open");
+}
 
-cardCover[4].addEventListener('click', function() {
-    cards[4].style.zIndex = 100;
-});
-
-cardCover[5].addEventListener('click', function() {
-    cards[5].style.zIndex = 100;
-});
